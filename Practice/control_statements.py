0dfb1f511f1396c12b962c38769a6fdfcc832ee9
G Yashasri <169059861+Yashasri-g@users.noreply.github.com>
2026-01-11 23:45:02 +0530

    Update control_statements.py

--- Practice/control_statements.py
+++ Practice/control_statements.py
@@ -1,1 +1,29 @@
+num = int(input("Enter a number: "))
+if num>0:
+    print("Positive number")
+elif num==0:
+    print("Zero")
+else:
+    print("Negative number")
+
+no = int(input("Enter a number: "))
+if no%2==0:
+    print("Even number")
+else:
+    print("odd number")
+
+age = int(input("Enter your age: "))
+if age>=18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible for vote")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+if num1>num2:
+    print(f"{num1} is greater")
+elif num1 == num2 :
+    print("Equal numbers")
+else:
+    print(f"{num2} is greater")
 
