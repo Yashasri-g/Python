cbdc3ec157f2dfe02917f4e96078cb45540cb697
G Yashasri <169059861+Yashasri-g@users.noreply.github.com>
2026-01-17 22:54:50 +0530

    Implement alternative anagram check solution
    Added an alternative solution for checking anagrams using character counting.

--- Leetcode/valid-anagram.py
+++ Leetcode/valid-anagram.py
@@ -12,3 +12,23 @@ class Solution:
                 return False
         else:
             return False
+
+
+# Solution 2 
+if len(s)!=len(t):
+            return False
+count = {}
+
+        # Count characters in s
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+
+        # Decrease count using t
+        for ch in t:
+            if ch not in count:
+                return False
+            count[ch] -= 1
+            if count[ch] < 0:
+                return False
+
+        return True
