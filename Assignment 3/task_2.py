757dfdef925c97ec0307c206b98358e8286f72f3
G Yashasri <169059861+Yashasri-g@users.noreply.github.com>
2026-01-19 12:10:55 +0530

    Fix sine calculation to use input number

--- Assignment 3/task_2.py
+++ Assignment 3/task_2.py
@@ -2,5 +2,5 @@ import math
 n = int(input("Enter a number: "))
 print(f"Square root : {math.sqrt(n)}")
 print(f"Logarithm: {math.log(n)}")
-print(f"Sine: {math.sin(x)}")
+print(f"Sine: {math.sin(n)}")
       
