2f3575d80a50cd2ee26622c6d43fbcd9c6ae1fde
G Yashasri <169059861+Yashasri-g@users.noreply.github.com>
2026-01-22 11:12:34 +0530

    Create final-value-of-variable-after-performing-operations.py

---
+++ Leetcode/final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,15 @@
+# 2011
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        x = 0 
+        for i in operations:
+            if i == "X++" or i == "++X":
+                x+=1
+            if i == "X--" or i == "--X":
+                x-=1
+        return x
+        
