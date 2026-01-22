a50c8e78298e4f337400caf34a9ca9f757adf557
G Yashasri <169059861+Yashasri-g@users.noreply.github.com>
2026-01-22 10:56:55 +0530

    Update Majority_Element.py

--- Leetcode/Majority_Element.py
+++ Leetcode/Majority_Element.py
@@ -2,3 +2,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         return statistics.mode(nums)
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d = {}
+        for item in nums:
+            if item in d:
+                d[item]+=1
+            else:
+                d[item] = 1
+        max_key = max(d, key=d.get)
+        return max_key
